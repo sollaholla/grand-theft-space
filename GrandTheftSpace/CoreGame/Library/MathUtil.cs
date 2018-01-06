82c0178929a26dcf478945efb78fac4227a4082a
Soloman N <sxnorthrop15@gmail.com>
2018-01-06 04:51:15 -0500

    Add mathamatic utility class.

---
+++ GrandTheftSpace/CoreGame/Library/MathUtil.cs
@@ -0,0 +1,28 @@
+﻿namespace GrandTheftSpace.CoreGame.Library
+{
+    internal static class MathUtil
+    {
+        public static float Lerp(float a, float b, float t)
+        {
+            return a + (b - a) * Clamp01(t);
+        }
+
+        public static float Clamp01(float value)
+        {
+            float result;
+            if (value < 0f)
+            {
+                result = 0f;
+            }
+            else if (value > 1f)
+            {
+                result = 1f;
+            }
+            else
+            {
+                result = value;
+            }
+            return result;
+        }
+    }
+}
